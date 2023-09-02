1ce932e7c137f7d4324352e696668e884f4791f4
Bùi Hải Triều <trieuql159@gmail.com>
2023-09-02 20:30:03 +0700

    hihih

--- src/screens/Home/Vm/LimitedSpending.tsx
+++ src/screens/Home/Vm/LimitedSpending.tsx
@@ -1,56 +1,187 @@
-import React, { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import {
+    KeyboardAvoidingView,
+    NativeSyntheticEvent,
+    Text,
+    TextInput,
+    TextInputChangeEventData,
+    View,
+} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import ButtonUI from '../../../components/Button';
 import { BG_SUB_COLOR } from '../../../utils/common';
 import { styles } from './LimitedSpendingStyle';
+const itemDropDownPicker = [
+    { label: 'Equally divided', value: 'equally' },
+    { label: 'Custom', value: 'custom' },
+];
 
 const LimitedSpending = () => {
+    const inputRef = useRef<TextInput | null>(null);
+    const [valueChangedTotal, setValueChangedTotal] = useState<number | any>('');
+    const [valueTotal, setValueTotal] = useState(0);
+    const [isTotal, setIsTotal] = useState(false);
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState('custom');
-    const [items, setItems] = useState([
-        { label: 'Equally divided', value: 'equally' },
-        { label: 'Food/Education', value: 'fe' },
-        { label: 'Custom', value: 'custom' },
-    ]);
+
+    /* handle channg Value Total */
+    const handleChangeTotalLimition = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+        setValueChangedTotal(Number(e.nativeEvent.text));
+    };
+
+    /* handle Save value total */
+    const handleSaveValueTotal = () => {
+        setIsTotal(true);
+        if (valueChangedTotal > 0) {
+            setValueTotal(valueChangedTotal);
+        }
+        setValueChangedTotal('');
+        if (inputRef.current) {
+            inputRef.current.blur();
+        }
+    };
+
     return (
-        <View style={{ marginTop: 6 }}>
-            {/* View select*/}
-            <View></View>
+        <KeyboardAvoidingView>
+            <View style={{ marginTop: 6 }}>
+                {/* View select*/}
+                <View></View>
 
-            {/* View total spending limit*/}
-            <View style={styles.view_container}>
-                <Text style={styles.text_field}>Limitation: </Text>
-                <TextInput placeholder="0.0" style={styles.input_plan} />
-                <TouchableOpacity style={styles.btn_save}>
-                    <Text style={{ color: 'white' }}>Save</Text>
-                </TouchableOpacity>
-            </View>
-
-            {/* View total spending limit*/}
-            <View style={[styles.view_container, { marginTop: 12 }]}>
-                <Text style={styles.text_field}>Total:</Text>
-                <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                    <Text style={[styles.text_total]}>100.000</Text>
-                    <MaterialIcons style={{ marginLeft: 4 }} name="attach-money" color={BG_SUB_COLOR} size={18} />
-                </View>
-                <View style={{ marginLeft: 28 }}>
-                    <DropDownPicker
-                        containerStyle={{ width: 200 }}
-                        dropDownContainerStyle={{ borderColor: BG_SUB_COLOR }}
-                        style={styles.dropdown_selected}
-                        open={open}
-                        value={value}
-                        items={items}
-                        setOpen={setOpen}
-                        setValue={setValue}
-                        setItems={setItems}
-                        showTickIcon={true}
+                {/* View total spending limit*/}
+                <View style={styles.view_container}>
+                    <Text style={styles.text_field}>Limitation: </Text>
+                    <TextInput
+                        ref={inputRef}
+                        onChange={(e) => handleChangeTotalLimition(e)}
+                        placeholder="0.0"
+                        style={styles.input_plan}
+                        keyboardType="numeric"
+                        value={valueChangedTotal.toString()}
                     />
+                    <TouchableOpacity
+                        disabled={!valueChangedTotal}
+                        onPress={handleSaveValueTotal}
+                        style={[styles.btn_save, !valueChangedTotal && styles.btn_save_disable]}
+                    >
+                        <Text style={{ color: valueChangedTotal ? 'black' : 'white' }}>Save</Text>
+                    </TouchableOpacity>
+                </View>
+
+                {/* View total spending limit*/}
+                <View style={[styles.view_container, { marginTop: 22 }]}>
+                    <Text style={styles.text_field}>Total:</Text>
+                    <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <Text style={[styles.text_total]}>
+                            {valueTotal.toLocaleString()}
+                            <MaterialIcons
+                                style={{ marginLeft: 4, marginTop: 6 }}
+                                name="attach-money"
+                                color={BG_SUB_COLOR}
+                                size={18}
+                            />
+                        </Text>
+                    </View>
+                    <View>
+                        <DropDownPicker
+                            containerStyle={{ width: 160 }}
+                            dropDownContainerStyle={{ borderColor: BG_SUB_COLOR }}
+                            style={styles.dropdown_selected}
+                            open={open}
+                            value={value}
+                            items={itemDropDownPicker}
+                            setOpen={setOpen}
+                            setValue={setValue}
+                            showTickIcon={true}
+                        />
+                    </View>
+                </View>
+
+                {/* View Category */}
+                <View style={[styles.view_container_column, { marginTop: 16 }]}>
+                    <Text style={[styles.text_field, { marginBottom: 10 }]}>Category Limitation</Text>
+                    <ScrollView style={{ maxHeight: 366 }}>
+                        <View style={[styles.view_category_item]}>
+                            <Text style={styles.text_category}>Food: </Text>
+                            <TextInput
+                                placeholder="0.0"
+                                style={[styles.input_plan, styles.input_plan_category]}
+                                keyboardType="numeric"
+                            />
+                        </View>
+                        <View style={[styles.view_category_item]}>
+                            <Text style={styles.text_category}>Shopping: </Text>
+                            <TextInput
+                                placeholder="0.0"
+                                style={[styles.input_plan, styles.input_plan_category]}
+                                keyboardType="numeric"
+                            />
+                        </View>
+                        <View style={[styles.view_category_item]}>
+                            <Text style={styles.text_category}>Gift: </Text>
+                            <TextInput
+                                placeholder="0.0"
+                                style={[styles.input_plan, styles.input_plan_category]}
+                                keyboardType="numeric"
+                            />
+                        </View>
+                        <View style={[styles.view_category_item]}>
+                            <Text style={styles.text_category}>Homeware: </Text>
+                            <TextInput
+                                placeholder="0.0"
+                                style={[styles.input_plan, styles.input_plan_category]}
+                                keyboardType="numeric"
+                            />
+                        </View>
+                        <View style={[styles.view_category_item]}>
+                            <Text style={styles.text_category}>Medical: </Text>
+                            <TextInput
+                                placeholder="0.0"
+                                style={[styles.input_plan, styles.input_plan_category]}
+                                keyboardType="numeric"
+                            />
+                        </View>
+                        <View style={[styles.view_category_item]}>
+                            <Text style={styles.text_category}>Education: </Text>
+                            <TextInput
+                                placeholder="0.0"
+                                style={[styles.input_plan, styles.input_plan_category]}
+                                keyboardType="numeric"
+                            />
+                        </View>
+                        <View style={[styles.view_category_item]}>
+                            <Text style={styles.text_category}>Exchange: </Text>
+                            <TextInput
+                                placeholder="0.0"
+                                style={[styles.input_plan, styles.input_plan_category]}
+                                keyboardType="numeric"
+                            />
+                        </View>
+                        <View style={[styles.view_category_item]}>
+                            <Text style={styles.text_category}>Invest: </Text>
+                            <TextInput
+                                placeholder="0.0"
+                                style={[styles.input_plan, styles.input_plan_category]}
+                                keyboardType="numeric"
+                            />
+                        </View>
+                        <View style={[styles.view_category_item]}>
+                            <Text style={styles.text_category}>Phone: </Text>
+                            <TextInput
+                                placeholder="0.0"
+                                style={[styles.input_plan, styles.input_plan_category]}
+                                keyboardType="numeric"
+                            />
+                        </View>
+                    </ScrollView>
+                </View>
+
+                <View style={styles.view_btn_submit}>
+                    <ButtonUI bgColor={BG_SUB_COLOR} text="Submit" onPress={() => {}} />
                 </View>
             </View>
-        </View>
+        </KeyboardAvoidingView>
     );
 };
 
--- src/screens/Home/Vm/LimitedSpendingStyle.ts
+++ src/screens/Home/Vm/LimitedSpendingStyle.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import { BG_SUB_COLOR, BUTTON_COLOR__HOME, FONT_FAMILY } from '../../../utils/common';
+import { BG_SUB_COLOR, BUTTON_COLOR__HOME, FONT_FAMILY, TEXT_COLOR_PRIMARY } from '../../../utils/common';
 
 export const styles = StyleSheet.create({
     view_container: {
@@ -8,9 +8,24 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         gap: 6,
     },
+    view_container_column: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    view_category_item: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 12,
+    },
+    view_btn_submit: {
+        marginTop: 80,
+        marginBottom: 16,
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
     dropdown_selected: {
         borderColor: BG_SUB_COLOR,
-        color: 'white',
     },
     input_plan: {
         backgroundColor: BUTTON_COLOR__HOME,
@@ -22,15 +37,27 @@ export const styles = StyleSheet.create({
         paddingBottom: 4,
         fontFamily: FONT_FAMILY,
         borderRadius: 6,
-        fontWeight: '900',
+        fontWeight: '600',
         fontSize: 17,
+    },
+    input_plan_category: {
+        width: 242,
     },
     btn_save: {
         paddingStart: 16,
         paddingEnd: 16,
         paddingTop: 8,
         paddingBottom: 8,
-        backgroundColor: BG_SUB_COLOR,
+        borderWidth: 1,
+        borderColor: BG_SUB_COLOR,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    btn_save_disable: {
+        backgroundColor: '#c8c8c8',
+        borderWidth: 0,
+        borderColor: TEXT_COLOR_PRIMARY,
         borderRadius: 4,
         justifyContent: 'center',
         alignItems: 'center',
@@ -41,10 +68,17 @@ export const styles = StyleSheet.create({
         color: 'black',
         fontWeight: '700',
     },
+    text_category: {
+        fontSize: 16,
+        color: TEXT_COLOR_PRIMARY,
+        fontWeight: '700',
+        width: 120,
+    },
     text_total: {
         marginLeft: 2,
         color: BG_SUB_COLOR,
         fontSize: 16,
         fontWeight: '700',
+        width: 150,
     },
 });
