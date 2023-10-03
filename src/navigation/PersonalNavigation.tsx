8269b0d948c87cb9aa4e7277fa34ac5160ebbe64
Haibt9702 <buihaibq9702@gmail.com>
2023-10-03 23:36:43 +0700

    merge main

--- src/navigation/PersonalNavigation.tsx
+++ src/navigation/PersonalNavigation.tsx
@@ -1,12 +1,7 @@
 import { SCREENS } from '../constants';
-<<<<<<< HEAD
-import { SettingScreen, ThemeScreen, ChangePassword, YearReport } from '../screens/PersonalScreen/Vm';
-=======
 import { SettingScreen, ThemeScreen, ChangePassword, YearReportScreen } from '../screens/PersonalScreen/Vm';
->>>>>>> sprint/logic-main
 
 export const PersonalNavigation = [
-    { name: SCREENS.YEAR_REPORT, component: YearReport },
     { name: SCREENS.SETTINGS, component: SettingScreen },
     { name: SCREENS.SETTING_THEME, component: ThemeScreen },
     { name: SCREENS.CHANGE_PASSORD, component: ChangePassword },
